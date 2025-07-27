b2e69c51f9328ebad77f683ecda6f2a439c227f1
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-07-27 19:33:58 +0530

    Minnor changes in user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,7 +3,11 @@ class UserModel {
   final String name;
   final String email;
 
-  UserModel({required this.id, required this.name, required this.email});
+  UserModel({
+    required this.id,
+     required this.name,
+      required this.email
+    });
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
         id: json['id'],
